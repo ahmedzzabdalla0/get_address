f47033313a662b12cc7778dae6a60082c5c34f66
A .M <ahmedzzabdalla0@gmail.com>
2023-10-01 03:40:47 +0300

    Structure

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+
+    print(request.remote_addr)
+
+    # url = f"http://api.ipstack.com/{request.remote_addr}"
+
+    # params={'access_key': '92a42fef93ee5935cc68c86ed0c3bc38'}
+
+    # req = requests.get(url, params=params)
+
+    # print(req.json())
+
+    return 'Hello from Flask!'
+
+
+app.run(host='0.0.0.0', port=81)
